41420673fee915821c30cbf3d960186c4a1bcca7
Nikita Mushchak <niky-m@yandex.ru>
2026-02-07 16:36:34 +0100

    IOS app initial commit

---
+++ PipesOptimizer/PipesOptimizer/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  PipesOptimizer
+//
+//  Created by Nikita Mushchak on 2026-02-07.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
